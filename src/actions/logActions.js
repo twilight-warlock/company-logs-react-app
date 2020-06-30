79ad10d1b56e832570424e4d644d037c0b22b95f
twilight-warlock <devansh.ts@somaiya.edu>
2020-07-01 04:05:25 +0530

    Added search functionality to SearchBar

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -7,6 +7,7 @@ import {
   SET_CURRENT,
   CLEAR_CURRENT,
   UPDATE_LOG,
+  SEARCH_LOGS,
 } from "./types";
 
 // export const getLogs = () => {
@@ -135,4 +136,24 @@ export const setLoading = () => {
   return {
     type: SET_LOADING,
   };
+};
+
+// Search for a log
+export const searchLogs = (text) => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs?q=${text}`);
+    const data = await res.json();
+
+    dispatch({
+      type: SEARCH_LOGS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    });
+  }
 };
