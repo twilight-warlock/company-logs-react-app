1873015537bf668775f0790bcea7508e45197ef9
twilight-warlock <devansh.ts@somaiya.edu>
2020-07-03 19:52:23 +0530

    Completed Company Logs project after adding Delete technician feature

--- src/actions/techActions.js
+++ src/actions/techActions.js
@@ -52,6 +52,27 @@ export const addTech = (tech) => async (dispatch) => {
   }
 };
 
+// Deletes a technician
+export const deleteTech = (id) => async (dispatch) => {
+  try {
+    setLoading();
+
+    await fetch(`/techs/${id}`, {
+      method: "DELETE",
+    });
+
+    dispatch({
+      type: DELETE_TECH,
+      payload: id,
+    });
+  } catch (err) {
+    dispatch({
+      type: TECHS_ERROR,
+      payload: err.response.statusText,
+    });
+  }
+};
+
 // Sets loading to true
 export const setLoading = () => {
   return {
--- src/components/techs/TechItem.js
+++ src/components/techs/TechItem.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { connect } from "react-redux";
+import { deleteTech } from "../../actions/techActions";
 import PropTypes from "prop-types";
+import M from "materialize-css/dist/js/materialize.min.js";
 
-const TechItem = ({ tech }) => {
+const TechItem = ({ tech, deleteTech }) => {
   return (
     <li className="collection-item">
       <div>
         {tech.firstName} {tech.lastName}
-        <a href="#@" className="secondary-content">
+        <a
+          href="#@"
+          className="secondary-content"
+          onClick={() => {
+            deleteTech(tech.id);
+            M.toast({ html: `Deleted ${tech.firstName} ${tech.lastName}` });
+          }}
+        >
           <i className="material-icons red-text">delete</i>
         </a>
       </div>
@@ -16,6 +26,7 @@ const TechItem = ({ tech }) => {
 
 TechItem.propTypes = {
   tech: PropTypes.object.isRequired,
+  deleteTech: PropTypes.func.isRequired,
 };
 
-export default TechItem;
+export default connect(null, { deleteTech })(TechItem);
